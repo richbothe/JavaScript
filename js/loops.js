3e98feed1109ab93cf55bfae4775e4d75df25e35
Rich Bothe <rich.bothe@metrotransit.org>
2021-08-08 17:04:55 -0500

    Loops.

--- js/loops.js
+++ js/loops.js
@@ -3,6 +3,16 @@ console.log('*** LOOPS ***');
 
 // The "for" loop
 var i;
-for (i = 0; i < 9; i = i + 1) {
+for (i = 0; i < 3; i = i + 1) {
     console.log(i);
+}
+// Shorter notation
+for (p = 0; p < 10; p++) {
+    console.log(p);
+}
+
+// Iterate over an array
+var myArray = ["A", "B", "C"];
+for (i = 0; i < myArray.length; i++) {
+    console.log("The member of myArray in index " + i + " is " + myArray[i]);
 }
